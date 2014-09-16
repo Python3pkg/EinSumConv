33b0050e72e60d085990f80d4754df021627ff45
Linda Linsefors <linda@linda-laptop>
2014-09-16 21:31:28 +0200

    Tillbaka till funkar

--- Tensors.py
+++ Tensors.py
@@ -1,68 +1,46 @@
-from sympy.core.function import Function, UndefinedFunction, AppliedUndef, Application
+from sympy.core.function import Function, UndefinedFunction, AppliedUndef
 from sympy.core.cache import cacheit
 from sympy.core.core import BasicMeta
 from sympy.core.sympify import sympify
-from sympy.core.symbol import Symbol
-from sympy.core.expr import Expr
 
-
-class Tensor(Function): 
+class Tensor(Function):
+    
     @cacheit
     def __new__(cls, *args, **options):
         # Handle calls like Tensor('F')
         if cls is Tensor:
             return UndefinedTensor(*args, **options)
-
-class TensorFunction(Function):  
-    @cacheit
-    def __new__(cls, *args, **options):
-        # Handle calls like TensorFunction('F')
-        if cls is TensorFunction:
-            return UndefinedTensorFunction(*args, **options)
         
 class UndefinedTensor(UndefinedFunction):
     """
     The (meta)class of undefined tensors.
     """
-    def __new__(mcl, name, **kwargs):
+    def __new__(mcl, name, **kwargs): #un-modified form parrent UndefindeFunction
         ret = BasicMeta.__new__(mcl, name, (AppliedTensor,), kwargs)
-        ret.__module__ = None
-        return ret 
-
-class UndefinedTensorFunction(UndefinedFunction):
-    """
-    The (meta)class of undefined tensors.
-    """
-    def __new__(mcl, name, **kwargs):
-        ret = BasicMeta.__new__(mcl, name, (AppliedTensorFunction,), kwargs)
         ret.__module__ = None
         return ret 
     
 
-#copy pased with UndefinedFunction -> UndefinedTensorFunction. I have no idea what this does
-UndefinedTensorFunction.__eq__ = lambda s, o: (isinstance(o, s.__class__) and 
+#copy pased with UndefinedFunction -> UndefinedTensor. I have no idea what this does
+UndefinedTensor.__eq__ = lambda s, o: (isinstance(o, s.__class__) and 
                                          (s.class_key() == o.class_key()))
-        
 
-class AppliedTensor(Tensor,AppliedUndef):
+class AppliedTensor(AppliedUndef):
+    """
+    Base class for expressions resulting from the application of an undefined
+    function.
+    """
+
     def __new__(cls, *args, **options):
         args = list(map(sympify, args))
         obj = super(AppliedTensor, cls).__new__(cls, *args, **options)
         return obj
-	@property
-	def index(self):
-		self.args
-
-class AppliedTensorFunction(TensorFunction,Tensor,AppliedUndef):  
-    def __new__(cls, *args, **options):
-        args = list(map(sympify, args))
-        obj = super(AppliedTensorFunction, cls).__new__(cls, *args, **options)
-        return obj
-	@property
-	def index(self):
-		self.args[0]
-	
-
+    
+    def __init__(self,*args,**options):
+        self.is_Tensor=True
+    
+    def index(self):
+        return self.args[0]
     
 
 '''
@@ -83,7 +61,12 @@ It is suggested to ouse the class Dummy for indecis and the class Symbol for ord
 #Special tesors
 
 class Delta(AppliedTensor):
-    pass
+    
+    def index(self):
+        return self.args
   
+    
 class Eps(AppliedTensor):
-	pass
+
+    def index(self):
+        return self.args
