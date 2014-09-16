6a8ffba91d0c7a1301c38ebe392029bc4a0e2eac
Linda Linsefors <linda@linda-laptop>
2014-09-16 22:45:29 +0200

    Det funkar nu

--- Tensors.py
+++ Tensors.py
@@ -4,7 +4,6 @@ from sympy.core.core import BasicMeta
 from sympy.core.sympify import sympify
 
 class Tensor(Function):
-    
     @cacheit
     def __new__(cls, *args, **options):
         # Handle calls like Tensor('F')
@@ -12,35 +11,32 @@ class Tensor(Function):
             return UndefinedTensor(*args, **options)
         
 class UndefinedTensor(UndefinedFunction):
-    """
-    The (meta)class of undefined tensors.
-    """
-    def __new__(mcl, name, **kwargs): #un-modified form parrent UndefindeFunction
+    def __new__(mcl, name, **kwargs):
         ret = BasicMeta.__new__(mcl, name, (AppliedTensor,), kwargs)
         ret.__module__ = None
         return ret 
     
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and (self.class_key() == other.class_key()))
 
 #copy pased with UndefinedFunction -> UndefinedTensor. I have no idea what this does
-UndefinedTensor.__eq__ = lambda s, o: (isinstance(o, s.__class__) and 
-                                         (s.class_key() == o.class_key()))
+#UndefinedTensor.__eq__ = lambda s, o: (isinstance(o, s.__class__) and 
+#   
 
 class AppliedTensor(AppliedUndef):
-    """
-    Base class for expressions resulting from the application of an undefined
-    function.
-    """
-
     def __new__(cls, *args, **options):
         args = list(map(sympify, args))
         obj = super(AppliedTensor, cls).__new__(cls, *args, **options)
         return obj
     
     def __init__(self,*args,**options):
+        self.index=args[0]
         self.is_Tensor=True
     
-    def index(self):
-        return self.args[0]
+
+    
+#    def index(self):
+#        return self.args[0]
     
 
 '''
