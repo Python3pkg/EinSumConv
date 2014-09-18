097e65d31bbbc99dae1738f4e5c65d3ac472d62f
Linda Linsefors <linda@linda-laptop>
2014-09-19 00:06:40 +0200

    Built subsIndex, it almost works

--- EinSumConv/renameIndex.py
+++ EinSumConv/renameIndex.py
@@ -31,9 +31,23 @@ def newIndexTermList(termList):
                                                      indexGenerator))
             for factorList
             in termList]
-    
-    
 
+
+
+def subsIndex(x,oldIndex,newIndex):
+    if not tensor.isTensor(x):
+        if not getattr(x, 'args', []):
+            return x
+        return type(x)(*[subsIndex(arg,oldIndex,newIndex) for arg in x.args])
+
+    indexList=list(x.index)
+    for (k,index) in enumerate(indexList):
+        if index==oldIndex or getattr(x, 'args', None)==oldIndex:
+            indexList[k]=newIndex
+    if not getattr(x, 'args', []):
+        return x.withNewIndex(*indexList)
+    return type(x)(*[subsIndex(arg,oldIndex,newIndex) 
+                     for arg in x.args]).withNewIndex(*indexList)
 
 
                 
