f70f2cc34f8ac02e3a6ec21ec10833dcfbd1659b
Linda Linsefors <linda@linda-laptop>
2014-09-20 23:26:05 +0200

    Rewritten tensors compleetly. I saved the old version temporay in a spearat file to easy see what functions I need to redefine.

--- EinSumConv/tensor.py
+++ EinSumConv/tensor.py
@@ -1,120 +1,47 @@
-from sympy.core.function import Function, UndefinedFunction, AppliedUndef, Application, FunctionClass
-from sympy.core.cache import cacheit
+import sympy.core.cache
+# import sympy.core.function
+# import sympy.core.compatibility
+
+# Why doesn't it work to use "import sympy.core.compatibility" and call "with_metaclass" as "sympy.core.compatibility.with_metaclass"?
+from sympy.core.compatibility import with_metaclass
+from sympy.core.function import AppliedUndef, FunctionClass
 from sympy.core.core import BasicMeta
-from sympy.core.sympify import sympify
-from sympy.core.expr import Expr
+from sympy.core.assumptions import ManagedProperties
 
 # this is the function used in other operations to check if something is a tensor.
 def isTensor(x):
-    return isinstance(x,Tensor)
+    return hasattr(x,'indices')
 
-class Tensor(Function):
-    @cacheit
-    def __new__(cls, *args, **options):
-        # Handle calls like Tensor('F')
-        if cls is Tensor:
-            return UndefinedTensor(*args, **options)
-    
-    __slots__ = ['_mhash',              # hash value
-                 '_args',               # arguments
-                 '_assumptions'
-                 'index'                # idecis
-                ]
+class TensorFunction(BasicMeta):
+    def __new__(mcl, name, *arg, **kw):
+        if (name == "AppliedTensorFunction"):
+            return type.__new__(mcl, name, *arg, **kw)
+        return type.__new__(mcl, name, (AppliedTensorFunction,), kw)
+    def __init__(self, *arg, **kw):
+        pass
 
-class UndefinedTensor(UndefinedFunction):
+class AppliedTensorFunction(FunctionClass):
+    __metaclass__ = TensorFunction
+    def __new__(mcl, *indices, **kw):
+        name = mcl.__name__ + str(indices)
+        ret = type.__new__(mcl, name, (AppliedUndef,),kw)
+        ret.indices = indices
+        return ret
 
-    def __new__(mcl, name, **kwargs):
-        ret = BasicMeta.__new__(mcl, name, (AppliedTensor,), kwargs)
-        ret.__module__ = None
+class Tensor(ManagedProperties):
+    def __new__(mcl, name, *arg, **kw):
+        if (name == "AppliedTensor"):
+            return type.__new__(mcl, name, *arg, **kw)
+        return type.__new__(mcl, name, (AppliedTensor,),kw)
+
+class AppliedTensor(sympy.Symbol):
+    __metaclass__ = Tensor
+    def __new__(cls, *indices, **kw):
+        name = cls.__name__ + str(indices)
+        ret = sympy.Symbol.__new__(cls, name, **kw)  
+        ret.indices = indices
         return ret
 
 
 
-#    def __eq__(self, other):
-#        return (isinstance(other, self.__class__) and (self.class_key() == other.class_key()))
 
-#copy pased with UndefinedFunction -> UndefinedTensor. I have no idea what this does
-#UndefinedTensor.__eq__ = lambda s, o: (isinstance(o, s.__class__) and 
-#   
-
-
-class AppliedTensor(AppliedUndef,Tensor):
-    def __new__(cls, *args):
-        obj = object.__new__(cls)
-        obj._assumptions = cls.default_assumptions
-        obj._mhash = None  # will be set by __hash__ method.
-
-        if isinstance(args[0], tuple):
-            obj.index = tuple(map(sympify, args[0]))
-            obj._args= tuple(map(sympify, args[1:]))
-        else:
-            obj._args = ()
-            obj.index = tuple(map(sympify, args))
-        return obj
-
-    def withNewIndex(self,*index):
-        if not self.args:
-            return type(self)(*index)
-        l=[index]
-        l.extend(self.args)
-        return type(self)(*l)
-        
-    @property
-    def indexAndArgs(self):
-        if not self.args:
-            return self.index
-        l=[self.index]
-        l.extend(self.args)
-        return tuple(l)
-
-    def __unicode__(self):
-        return type(self).__name__ + unicode(self.indexAndArgs)
-
-#    def __str__(self):
-#        return type(self).__name__ + str(self.indexAndArgs)
-
-#    def __repr__(self):
-#        return type(self).__name__ + repr(self.indexAndArgs)
-
-    def __eq__(self, other):
-        return type(self)==type(other) and self.indexAndArgs==other.indexAndArgs
-
-    def __hash__(self):
-        return hash(str(self))
-
-'''
-Tensor is a copy of the contruction of Function with the addition .index() and .is_tensor
-
-User guide: Put the indecis as an tupple, as the rist index in the TensorFunktion
-
-Example1: To get the tensor $T_{a,b}$, witie:
->>> T=Tenosr('T')
->>> T((a,b))
-Example2: To get the tensor $T_{a,b}(x,y)$, witie:
->>> T=Tenosr('T')
->>> T((a,b),x,y)
-
-It is suggested to ouse the class Dummy for indecis and the class Symbol for ordinary variables, to keep them appart.
-'''
-
-
-
-import sympy.printing.str
-
-def stringPrinter_Tensor(self,expr):
-    return type(expr).__name__ + str(expr.indexAndArgs)
-
-sympy.printing.str.StrPrinter._print_Tensor = stringPrinter_Tensor
-
-# in ReprPrinter
-#    def _print_Function(self, expr):
-#        r = self._print(expr.func)
-#        r += '(%s)' % ', '.join([self._print(a) for a in expr.args])
-#        return r
-
-# in StringPrinter 
-#   def _print_Function(self, expr):
-#        return expr.func.__name__ + "(%s)" % self.stringify(expr.args, ", ")
-
-# sympy.printing.repr.ReprPrinter._print_Function = reprPrinter_Tensor
-    
