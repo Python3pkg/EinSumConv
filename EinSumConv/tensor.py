f005a53723d99fc140e40efc2bce722dd2c1000b
Linda Linsefors <linda@linda-laptop>
2014-09-20 23:57:01 +0200

    Fixed equvalence and isTensor

--- EinSumConv/tensor.py
+++ EinSumConv/tensor.py
@@ -7,12 +7,15 @@ from sympy.core.compatibility import with_metaclass
 from sympy.core.function import AppliedUndef, FunctionClass
 from sympy.core.core import BasicMeta
 from sympy.core.assumptions import ManagedProperties
+from sympy.core.cache import cacheit
+
 
 # this is the function used in other operations to check if something is a tensor.
 def isTensor(x):
-    return hasattr(x,'indices')
+    return getattr(x,'is_Tensor',False)
 
 class TensorFunction(BasicMeta):
+    @cacheit
     def __new__(mcl, name, *arg, **kw):
         if (name == "AppliedTensorFunction"):
             return type.__new__(mcl, name, *arg, **kw)
@@ -22,13 +25,20 @@ class TensorFunction(BasicMeta):
 
 class AppliedTensorFunction(FunctionClass):
     __metaclass__ = TensorFunction
+
+    @cacheit
     def __new__(mcl, *indices, **kw):
         name = mcl.__name__ + str(indices)
         ret = type.__new__(mcl, name, (AppliedUndef,),kw)
         ret.indices = indices
         return ret
+    is_Tensor = True
+
+    def withNewIndex(self,*indices):
+        return type(type(self))(*indices)(*self.args)
 
 class Tensor(ManagedProperties):
+    @cacheit
     def __new__(mcl, name, *arg, **kw):
         if (name == "AppliedTensor"):
             return type.__new__(mcl, name, *arg, **kw)
@@ -36,11 +46,17 @@ class Tensor(ManagedProperties):
 
 class AppliedTensor(sympy.Symbol):
     __metaclass__ = Tensor
+
+    @cacheit
     def __new__(cls, *indices, **kw):
         name = cls.__name__ + str(indices)
         ret = sympy.Symbol.__new__(cls, name, **kw)  
         ret.indices = indices
         return ret
+    is_Tensor = True
+
+    def withNewIndex(self,*indices):
+        return type(self)(*indices)
 
 
 
