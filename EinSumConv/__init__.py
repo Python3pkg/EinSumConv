d4efaddb897788dfbf280501f02b00c041a4cb40
Linda Linsefors <linda@linda-laptop>
2014-09-25 12:41:55 +0200

    Written a to do list

--- EinSumConv/__init__.py
+++ EinSumConv/__init__.py
@@ -5,3 +5,12 @@ def printStructure(x):
         return type(x).__name__ + "(" + ", ".join([printStructure(xa) for xa in x.args]) + ")"
     else:
         return str(x)
+
+
+
+# To doo list
+'''
++ Write unittest for EVERYTHING! 
++ Handle symetric and anti-symetric tensors
++ Fix pretty print
+'''
