24cadd65c17b8600761b0ce0bf8c17447a0da9a1
Linda Linsefors <linda@linda-laptop>
2014-09-25 12:42:51 +0200

    Written comments and poked around

--- EinSumConv/lists.py
+++ EinSumConv/lists.py
@@ -2,15 +2,19 @@ import sympy
 import tensor
 
 
-def printStructure(x):
-    if getattr(x, 'args', []):
-        return (type(x).__name__ 
+# gives a sring that showes the structure of an expression in sympy
+# (This was used to examine sympy, but is not acctually used in any EinSumConv functions)
+def printStructure(exp):
+    if getattr(exp, 'args', []):
+        return (type(exp).__name__ 
                 + "(" 
-                + ", ".join([printStructure(xa) for xa in x.args]) 
+                + ", ".join([printStructure(arg) for arg in exp.args]) 
                 + ")")
     else:
-        return str(x)
+        return str(exp)
 
+
+# Flatens a list (found on internet)
 def flatten(x):
     result = []
     for el in x:
@@ -21,32 +25,38 @@ def flatten(x):
     return result
 
 
-def makeFactorList(x):
-    if (isinstance(x,sympy.Pow) 
-            and isinstance(x.args[1],(int,sympy.Integer)) 
-            and x.args[1]>1 ):
+# factorList is a list of all factors in an expression
+def makeFactorList(exp):
+    if (isinstance(exp,sympy.Pow) 
+            and isinstance(exp.args[1],(int,sympy.Integer)) 
+            and exp.args[1]>1 ):
         factorList=[]
-        base=makeFactorList(x.args[0])
-        for i in range(x.args[1]):
+        base=makeFactorList(exp.args[0])
+        for i in range(exp.args[1]):
             factorList.append(base)
         return flatten(factorList)
-    
-    if isinstance(x,sympy.Mul):
+    if isinstance(exp,sympy.Mul):
         factorList=[]
-        for factor in x.args:
+        for factor in exp.args:
             factorList.append(makeFactorList(factor))
         return flatten(factorList)
-    return [x]
+    return [exp]
     
-def makeTermList(x):
-    if isinstance(x,sympy.Mul) or isinstance(x,sympy.Pow):
-        return[makeFactorList(x)]
-    if not isinstance(x,sympy.Add):
-        return [[x]]
-    return [makeFactorList(term) for term in x.args]
+
+# termList is a list of factorList for each arg of a sympy.Add
+# makeTermList respects comutations rules by not moving stuff around
+def makeTermList(exp):
+    if isinstance(exp,sympy.Mul) or isinstance(exp,sympy.Pow):
+        return[makeFactorList(exp)]
+    if not isinstance(exp,sympy.Add):
+        return [[exp]]
+    return [makeFactorList(term) for term in exp.args]
 
 
-def serchIndexInFactor(exp,indexList=None,indexPos=0):
+# Find indecis on all levels in an expression.
+# indexList is a list of all indecis
+# indexDict is a dictionary that maches the possition in indexList with the possiton of that index in the expression.
+def serchIndexInFactor(exp, indexList=None, indexPos=0):
     if indexList is None:
         indexList = []
     indexDict = {}
@@ -67,6 +77,8 @@ def serchIndexInFactor(exp,indexList=None,indexPos=0):
     return indexDict, indexList
 
 
+# Takes an expression, an indexDict as the one created by serchIndexInFactor, and list of (new?) indecis.
+# Builds a new expression, same as factor, but with the indecis in indexList
 def rebuildFactor(factor,indexList,indexDict):
     if tensor.isTensor(factor): 
         factor = tensor.withNewIndex(
@@ -80,7 +92,10 @@ def rebuildFactor(factor,indexList,indexDict):
         factor = type(factor)(*argsList)
     return factor
             
-                    
+
+# TensorFactorList is a list of all tensors (and functions of tensor) that where in the factorList
+# respects comutation rules by saving the possition in factorList of all tensors.
+# TensorFactorList is usefull for playing with indecis
 def makeTensorFactorList(factorList):
     ret=[]
     for (i,factor) in enumerate(factorList):
@@ -94,24 +109,25 @@ def makeTensorFactorList(factorList):
     return ret
 
 
+# Takes a TensorFactorList and it's original factorList and puts back the tensors
 def rebuildMul(tensorFactorList,factorList):
     newFactorList = list(factorList)    
     for factor in tensorFactorList:
         newFactorList[factor[0]] = rebuildFactor(factor[3],factor[2],factor[4])
     return sympy.Mul(*newFactorList)
 
-        
+
+# tenosrTermList is a list of TensorFactorList for each term
 def makeTensorTermList(termList):
     return [makeTensorFactorList(factorList) for factorList in termList]
 
 
+# Takes a TensorTermList and it's original termList and puts back the tensors
 def rebuildAdd(tensorTermList,TermList):
     return sympy.Add(*[rebuildMul(tfl,TermList[i]) 
                        for (i,tfl) in enumerate(tensorTermList)])
         
         
-    
-
 def indexPathern(index):
     d = {}
     n = 0
@@ -122,6 +138,8 @@ def indexPathern(index):
             n = n+1
         pathern.append(d[ind])
     return pathern
+
+
 
 def comprTensorsInList(A,B):
     if A[1] == B[1]: 
@@ -136,19 +154,22 @@ def comprTensorsInList(A,B):
     if A[1] > B[1]: return 1
     return 0
 
+
 def sortTensorTermList(tensorTermList):
     for tensorFactorList in tensorTermList:
         tensorFactorList.sort(comprTensorsInList)
 
-def printList(list):
-    l=len(list)
+
+# print a list, one element per row, for easier reeding.
+def printList(lis):
+    l=len(lis)
     if l>2:
-        print list
+        print lis
         return None
-    print '[ ' + str(list[0]) + ' ,'
+    print '[ ' + str(lis[0]) + ' ,'
     for i in range(1,l-1):
-        print '  ' + str(list[i]) + ' ,'
-    print '  ' + str(list[l-1]) + ' ]'
+        print '  ' + str(lis[i]) + ' ,'
+    print '  ' + str(lis[l-1]) + ' ]'
     return None
     
 
